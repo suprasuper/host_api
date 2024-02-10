b0dedc38921e8b4dd5e1d03fc6bc5afad9e47595
suprasuper <134078308+suprasuper@users.noreply.github.com>
2024-02-10 03:03:03 +0100

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ const players = [];
 
 
 const app = express();
-app.use(cors());
+const options = {
+  origin: 'http://127.0.0.1:5500',
+}
+app.use(cors(options));
 
 const API_KEY = "RGAPI-faeead12-e326-47cc-8bc3-a3744ab2f0de"
 
@@ -17,7 +20,6 @@ function getPLayer(id) {
 }
 
 app.get('/allLeagues',async(req,res)=>{
-  res.set('Access-Control-Allow-Origin', '*');
   const ids = ["EdlXilkFBeCqLWp4ueTZKjHBulsPyjrev1Mxg2_2yCHtqNM","NooO92s_pLaIkxu1MfQ0JzevllBzPReNlVjCfUZMwt5e3T8","dEdvO_yJpEBWtoOrR5ML-3oydz45lyA26FaH430rNazDDfU","kBWF56OOLJ6cmolafDqtEWsupL0lwtb8Z-Vb6GJJGZIc2b8","KXdujJcXyRwtC49D-ir2O4lGKmgZdbkXQ_mNJ58FuNryc_Xl","6W2lrvTtLClCgxAvHgon13AqmC6vCkY9LkwLVZqCrN-lrF0","An0DMGGlDEh1jS5XloAmIxbRdae82gLz02LHVjZKgYRcjW0L","wY5fu_yH2msVoZGdoyz1mn3nsMOVwEltn-0kvpODWcjEqPoQy7hoQByqqg"]
   for(const id of ids) {
     const player = await getPLayer(id)
