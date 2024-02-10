d2eaeeea652eeb33da3b897961b26f755cfb2976
suprasuper <134078308+suprasuper@users.noreply.github.com>
2024-02-10 02:33:11 +0100

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(cors());
 
 const API_KEY = "RGAPI-faeead12-e326-47cc-8bc3-a3744ab2f0de"
 
-app.get((req, res, next) => {
+app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
